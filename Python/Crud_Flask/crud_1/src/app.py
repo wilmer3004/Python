6dcb06a5f5c2ed950353864ffb84c06a4e0d8919
wilmer3004 <131203913+wilmer3004@users.noreply.github.com>
2023-07-06 23:22:20 -0500

    Se  realizo una crud con flask
    Para trabajar con esta crud desde github tendremos que meternos dentro de mas capetas dentro del controlador

---
+++ Python/Crud_Flask/crud_1/src/app.py
@@ -0,0 +1,82 @@
+from flask import Flask, render_template, request, redirect, url_for
+import os 
+import database as db
+
+template_dir = os.path.dirname(os.path.abspath(os.path.dirname(__file__)))
+template_dir = os.path.join(template_dir, 'src','templates')
+
+app = Flask(__name__ ,template_folder = template_dir)
+
+# Rutas de la aplicación
+
+@app.route('/')
+def home():
+    cursor = db.database.cursor()
+    cursor.execute("SELECT * FROM usuario")
+    myresult = cursor.fetchall()
+    # convertir lo datos a diccionario
+    insertObject = []
+    columnNames = [column[0] for column in cursor.description]
+    for record in myresult:
+        insertObject.append(dict(zip(columnNames,record)))
+    cursor.close()
+    return render_template('index.html', data=insertObject)
+
+
+# Ingresar usuarios en la base de datos
+
+@app.route('/user', methods=['POST'])
+def addUser():
+    username = request.form['username']
+    password = request.form['password']
+    
+    if username and password:
+        cursor = db.database.cursor()
+        sql = "INSERT INTO usuario (NombreUsuario, ContrasenaUsuario) VALUES (%s,%s)"
+        data = (username, password)
+        cursor.execute(sql,data)
+        db.database.commit()
+    return redirect(url_for('home'))
+        
+# -------------------------------------------
+# Eliminar
+@app.route('/delete/<string:id>')
+
+def delete(id):
+    cursor = db.database.cursor()
+    sql = "DELETE FROM usuario WHERE IdUsuario=%s"
+    data = (id,)
+    cursor.execute(sql,data)
+    db.database.commit()
+    return redirect(url_for('home'))
+
+# -------------------------------------------
+# Editar
+@app.route('/edit/<string:id>',methods=['POST'])
+
+def edit(id):
+    username = request.form['username']
+    password = request.form['password']
+    
+    if username and password:
+        cursor = db.database.cursor()
+        sql = "UPDATE usuario SET NombreUsuario = %s, ContrasenaUsuario= %s WHERE IdUsuario = %s"
+        data = (username, password,id)
+        cursor.execute(sql,data)
+        db.database.commit()
+    return redirect(url_for('home'))
+
+
+# -------------------------------------------
+
+
+if __name__ == '__main__':
+    app.run(debug=True, port=4000)
+
+
+# -------------------------------------------------------------
+
+
+
+
+
