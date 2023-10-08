4b30a967da04fcdaa37a27a9fbedb2df01fa3272
wilmer3004 <131203913+wilmer3004@users.noreply.github.com>
2023-10-08 00:05:14 -0500

    status vercel

--- Python/proyecto_vercel/api/main.py
+++ Python/proyecto_vercel/api/main.py
@@ -4,16 +4,15 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "HELLO from vercel use flask"
+    try:
+        # código de tu función
+        return "HELLO from vercel use flask"
+    except Exception as e:
+        # registra el error o imprímelo para depuración
+        print(str(e))
+        return "Error interno del servidor", 500
 
 @app.route("/about")
 def about():
+    # código de tu función
     return "about"
-
-
-if __name__ == "__main__":
-    app.run()
-
-
-
-
